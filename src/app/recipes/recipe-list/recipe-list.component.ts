aa77f319e2869bea9c594c89be18cb94587c7727
nilgupta <44253057+nilgupta@users.noreply.github.com>
2023-05-08 18:57:43 +0530

    creating ingredient model and recipe details page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/model/recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes :Recipe[] = [
+    new Recipe('Test Recipe1',"This is simply a test","https://static.onecms.io/wp-content/uploads/sites/43/2022/03/20/212721-Indian-Chicken-Curry-Murgh-Kari-mfs_005.jpg"),
+    new Recipe('Test Recipe2',"This is simply a test2","https://static.onecms.io/wp-content/uploads/sites/43/2022/03/20/212721-Indian-Chicken-Curry-Murgh-Kari-mfs_005.jpg")
+  ];
   constructor() { }
 
   ngOnInit(): void {
